44b10f62a71410af4bfd4c1e5eba1fa35c748806
Seth Donaldson <sethd63@gmail.com>
2021-04-06 16:55:34 -0400

    update maxiter

--- demo/test.py
+++ demo/test.py
@@ -991,7 +991,7 @@ def run():
 
     snapshot = 'snapshot-step0-final--0'  # snapshot for step 1
 
-    fit_dgp(snapshot, dlcpath, shuffle=shuffle, step=step, batch_size=batch_size, maxiters=50000)
+    fit_dgp(snapshot, dlcpath, shuffle=shuffle, step=step, batch_size=batch_size, maxiters=200000)
 
 if __name__ == '__main__':
     run()
