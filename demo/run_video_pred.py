8e379d7c3dccdf6af92bacb40b03eb0bc6479bbe
Long Nguyen <long.nguyen11288@gmail.com>
2021-09-15 09:51:53 -0400

    mp4v

--- demo/run_video_pred.py
+++ demo/run_video_pred.py
@@ -242,7 +242,7 @@ if __name__ == '__main__':
         videooutname = filename.split(".")[0] + "_dgp_labeled.mp4"
         print("VIDEO OUT NAME")
         print(videooutname)
-        fourcc = cv2.VideoWriter_fourcc(*'mpeg')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         clip = vp(fname=video_file,sname=videooutname,codec=fourcc)
         filepath = filename.split(".")[0] + "_labeled.h5"
         df = pd.read_hdf(filepath)
